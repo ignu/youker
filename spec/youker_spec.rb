db5e7dc979c4350ab4dd32ed854966490c684dfc
Len Smith <ignu@soundwave.local>
2009-12-08 16:44:05 -0500

    using the layout template

--- spec/youker_spec.rb
+++ spec/youker_spec.rb
@@ -5,11 +5,20 @@ require 'rack/test'
 describe "Application" do
     include Rack::Test::Methods
     
-    
-    it "should respond with ok" do
-      get "/"
-      last_response.ok?.should be true
-      last_response.body.should match "Quick Schedule"
+    describe "loading the root url" do
+      before(:all) do
+        get "/"
+      end
+      
+      it "should responsd with the index template" do
+        last_response.ok?.should be true
+        last_response.body.should match "Quick Schedule"
+      end
+      
+      it "should use the application template" do
+        last_response.body.should match "Schedule Generator"
+      end
+            
     end
     
 end
